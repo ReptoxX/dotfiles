4057547261f80c0ee9b510bcf908b67aa589c5b5
Julien Terzibaschian <jt@e-confirm.de>
2023-07-16 02:13:35 +0200

    feat: added windows support to wezterm

--- wezterm.lua
+++ wezterm.lua
@@ -7,13 +7,29 @@ if wezterm.config_builder then
 end
 
 
-
 config.color_scheme = 'catppuccin-frappe'
 config.font = wezterm.font(
-  "JetbrainsMono Nerd Font Mono",
+  "JetBrainsMono Nerd Font",
   { weight = "Regular" }
 )
-config.font_size = 14
+config.font_size = 12
 config.freetype_load_target = "Normal"
+
+config.window_frame = {
+  font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Regular" }),
+  font_size = 8
+}
+
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 2,
+  bottom = 0
+}
+
+config.hide_tab_bar_if_only_one_tab = true
+
+-- Windows specific
+config.default_domain = "WSL:Ubuntu-22.04"
 
 return config
