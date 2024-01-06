5828ca5208cd872c2571f9fbfa95244b00ac6d15
Julien Terzibaschian <jt@e-confirm.de>
2024-01-06 18:46:17 +0100

    refactor(nvim): moved autocmd to correct place

--- config/nvim/lua/plugins/templ.lua
+++ config/nvim/lua/plugins/templ.lua
@@ -2,15 +2,6 @@ vim.filetype.add({
   extension = { templ = "templ" },
 })
 
-vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-  pattern = "*.templ",
-  desc = "Run compiler on save",
-  callback = function()
-    local file_name = vim.api.nvim_buf_get_name(0)
-    vim.cmd("silent !templ generate -f " .. file_name)
-  end,
-})
-
 return {
   {
     "stevearc/conform.nvim",
