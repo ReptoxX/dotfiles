b52c8c2210f91b3eec7b8f95c87510fb02bc8f3b
Julien Terzibaschian <jt@e-confirm.de>
2024-08-27 23:29:04 +0200

    chore: changed gitLiveStableInAll Func

--- gitFunctions.sh
+++ gitFunctions.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-function func_gitLiveStableAllIntranet () {
+function func_gitLiveStableAllIntranet() {
     func_gitLiveStableIn tvg
     func_gitLiveStableIn tc
     func_gitLiveStableIn alpha
-    func_gitLiveStableIn galeria
+    func_gitLiveStableIn tts
+    func_gitLiveStableIn ft
+    func_gitLiveStableIn ftch
     func_gitCheckout test
     git merge origin/live_stable
     git status
@@ -12,34 +14,37 @@ function func_gitLiveStableAllIntranet () {
     func_continueMerge
 }
 
-function func_gitLiveStableAll () {
+function func_gitLiveStableAll() {
 
-	func_gitLiveStableIn tvg
-    	func_gitLiveStableIn alpha
-    	func_gitLiveStableIn econfirm
-    	func_gitLiveStableIn rtk
-    	func_gitLiveStableIn expitv
-	func_gitLiveStableIn tc
-    	func_gitLiveStableIn wootb
-    	func_gitLiveStableIn dtps
-    	func_gitLiveStableIn tf
-#    func_gitLiveStableIn galeria
-#    func_gitLiveStableIn karstadt
-#    func_gitLiveStableIn tuishopfinder
+    func_gitLiveStableIn tvg
+    func_gitLiveStableIn alpha
+    func_gitLiveStableIn econfirm
+    func_gitLiveStableIn rtk
+    func_gitLiveStableIn expitv
+    func_gitLiveStableIn tc
+    func_gitLiveStableIn wootb
+    func_gitLiveStableIn dtps
+    func_gitLiveStableIn tf
+    func_gitLiveStableIn tts
+    func_gitLiveStableIn ft
+    func_gitLiveStableIn ftch
 }
 
-function func_continueMerge () {
+function func_continueMerge() {
     while true; do
-        echo "Mit nächstem live Branch fortfahren? [Y/n] "
+        echo "Mit nächstem live Branch fortfahren? Bitte überprüfe alle Änderungen und füge sie zum commit hinzu. [Y/n] "
         read yn
         case $yn in
-            [Nn]* ) exit 1;;
-            [Yy]* | * ) git push; func_gitAddCommitPush; break;;
+        [Nn]*) exit 1 ;;
+        [Yy]* | *)
+            func_gitAddCommitPush
+            break
+            ;;
         esac
     done
 }
 
-function func_gitLiveStableIn () {
+function func_gitLiveStableIn() {
     func_gitCheckout live_"$1"
     git merge origin/live_stable
     git status
@@ -47,11 +52,11 @@ function func_gitLiveStableIn () {
     func_continueMerge
 }
 
-function func_gitCheckout () {
+function func_gitCheckout() {
     git checkout "$1" && git fetch && git pull
 }
 
-function func_gitAddCommit () {
+function func_gitAddCommit() {
     if [ -z "$1" ]; then
         git add . && git commit
     else
@@ -59,11 +64,6 @@ function func_gitAddCommit () {
     fi
 }
 
-function func_gitAddCommitPush () {
-    if [ -z "$1" ]; then
-        git add . && git commit && git push
-    else
-        git add . && git commit -m "$1" && git push
-    fi
+function func_gitAddCommitPush() {
+    git add . && git commit --no-edit && git push
 }
-
